91bbac156cdeafcbd35701c555543e318dce1955
jorge <jorge.andre.silva.fernandes@gmail.com>
2023-12-09 13:41:01 +0000

    Add register form.

--- website/views.py
+++ website/views.py
@@ -35,3 +35,7 @@ def logout_user(request):
     logout(request)
     messages.success(request, "You have been logged out.")
     return redirect("home")
+
+
+def register_user(request):
+    return render(request, "register.html", {})
